72abcea2f5b8e641d324e804c1dd8ca11d4d88d6
Matthew Casperson <matthewcasperson@gmail.com>
2024-02-19 07:24:26 +1000

    Added more security checks

---
+++ internal/checks/security/octopus_insecure_k8s_check.go
@@ -0,0 +1,65 @@
+package security
+
+import (
+	"errors"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/client"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/machines"
+	"github.com/OctopusSolutionsEngineering/OctopusRecommendationEngine/internal/checks"
+	"github.com/samber/lo"
+	"strings"
+)
+
+// OctopusInsecureK8sCheck checks to see if any targets have not been used in a month
+type OctopusInsecureK8sCheck struct {
+	client       *client.Client
+	errorHandler checks.OctopusClientErrorHandler
+}
+
+func NewOctopusInsecureK8sCheck(client *client.Client, errorHandler checks.OctopusClientErrorHandler) OctopusInsecureK8sCheck {
+	return OctopusInsecureK8sCheck{client: client, errorHandler: errorHandler}
+}
+
+func (o OctopusInsecureK8sCheck) Id() string {
+	return "OctoLintInsecureK8sTargets"
+}
+
+func (o OctopusInsecureK8sCheck) Execute() (checks.OctopusCheckResult, error) {
+	if o.client == nil {
+		return nil, errors.New("octoclient is nil")
+	}
+
+	targets, err := o.client.Machines.GetAll()
+
+	if err != nil {
+		return o.errorHandler.HandleError(o.Id(), checks.Security, err)
+	}
+
+	k8sTargets := lo.Filter(targets, func(item *machines.DeploymentTarget, index int) bool {
+		return item.Endpoint.GetCommunicationStyle() == "Kubernetes"
+	})
+
+	insecureMachines := []string{}
+	for _, m := range k8sTargets {
+		k8sEndpoint := m.Endpoint.(*machines.KubernetesEndpoint)
+		if k8sEndpoint.SkipTLSVerification || (k8sEndpoint.ClusterURL != nil && strings.HasPrefix(k8sEndpoint.ClusterURL.String(), "http://")) {
+			insecureMachines = append(insecureMachines, m.Name)
+		}
+
+	}
+
+	if len(insecureMachines) > 0 {
+		return checks.NewOctopusCheckResultImpl(
+			"The following Kubernetes skip TLS validation or use an insecure HTTP endpoint:\n"+strings.Join(insecureMachines, "\n"),
+			o.Id(),
+			"",
+			checks.Warning,
+			checks.Security), nil
+	}
+
+	return checks.NewOctopusCheckResultImpl(
+		"There are no insecure Kubernetes targets",
+		o.Id(),
+		"",
+		checks.Ok,
+		checks.Security), nil
+}
---
+++ internal/checks/security/octopus_insecure_subscriptions_check.go
@@ -0,0 +1,70 @@
+package security
+
+import (
+	"errors"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/client"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/resources"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/services/api"
+	"github.com/OctopusSolutionsEngineering/OctopusRecommendationEngine/internal/checks"
+	"strings"
+)
+
+// OctopusInsecureSubscriptionsCheck checks to see if any targets have not been used in a month
+type OctopusInsecureSubscriptionsCheck struct {
+	client       *client.Client
+	errorHandler checks.OctopusClientErrorHandler
+}
+
+func NewOctopusInsecureSubscriptionsCheck(client *client.Client, errorHandler checks.OctopusClientErrorHandler) OctopusInsecureSubscriptionsCheck {
+	return OctopusInsecureSubscriptionsCheck{client: client, errorHandler: errorHandler}
+}
+
+func (o OctopusInsecureSubscriptionsCheck) Id() string {
+	return "OctoLintInsecureWebhookUrls"
+}
+
+func (o OctopusInsecureSubscriptionsCheck) Execute() (checks.OctopusCheckResult, error) {
+	if o.client == nil {
+		return nil, errors.New("octoclient is nil")
+	}
+
+	collection := resources.Resources[*OctopusSubscription]{}
+	_, err := api.ApiGet(o.client.Subscriptions.GetClient(), &collection, o.client.Subscriptions.BasePath+"?skip=0&take=2147483647")
+
+	if err != nil {
+		return o.errorHandler.HandleError(o.Id(), checks.Security, err)
+	}
+
+	insecureItems := []string{}
+	for _, m := range collection.Items {
+		if m.EventNotificationSubscription != nil && strings.HasPrefix(m.EventNotificationSubscription.WebhookURI, "http://") {
+			insecureItems = append(insecureItems, m.Name)
+		}
+
+	}
+
+	if len(insecureItems) > 0 {
+		return checks.NewOctopusCheckResultImpl(
+			"The following subscriptions use an insecure HTTP webhook URL:\n"+strings.Join(insecureItems, "\n"),
+			o.Id(),
+			"",
+			checks.Warning,
+			checks.Security), nil
+	}
+
+	return checks.NewOctopusCheckResultImpl(
+		"There are no insecure subscriptions",
+		o.Id(),
+		"",
+		checks.Ok,
+		checks.Security), nil
+}
+
+type OctopusSubscription struct {
+	Name                          string
+	EventNotificationSubscription *OctopusEventNotificationSubscription
+}
+
+type OctopusEventNotificationSubscription struct {
+	WebhookURI string
+}
