1638aaa6e7a82715c55fcd774993aec094caf707
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-25 19:08:38 +1000

    Test with a few more targets

--- test/terraform/31-insecurek8s/cloud_region.tf
+++ test/terraform/31-insecurek8s/cloud_region.tf
@@ -13,3 +13,19 @@ resource "octopusdeploy_cloud_region_deployment_target" "target_region1" {
   tenants                           = []
   thumbprint                        = ""
 }
+
+resource "octopusdeploy_offline_package_drop_deployment_target" "target_offlineoffline" {
+  applications_directory            = "c:\\temp"
+  working_directory                 = "c:\\temp"
+  environments                      = ["${octopusdeploy_environment.development_environment.id}"]
+  name                              = "Offline"
+  roles                             = ["offline"]
+  health_status                     = "Healthy"
+  is_disabled                       = false
+  machine_policy_id                 = "${data.octopusdeploy_machine_policies.default_machine_policy.machine_policies[0].id}"
+  shell_name                        = "Unknown"
+  shell_version                     = "Unknown"
+  tenant_tags                       = []
+  tenanted_deployment_participation = "Untenanted"
+  tenants                           = []
+}
