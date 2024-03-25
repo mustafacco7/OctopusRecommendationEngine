4d7ebe540f2d01dbeab355f64d4b00ef63377f3b
Matthew Casperson <matthewcasperson@gmail.com>
2024-03-25 18:58:24 +1000

    Added a test

---
+++ internal/checks/security/octopus_insecure_k8s_check_test.go
@@ -0,0 +1,51 @@
+package security
+
+import (
+	"errors"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/client"
+	"github.com/OctopusSolutionsEngineering/OctopusRecommendationEngine/internal/checks"
+	"github.com/OctopusSolutionsEngineering/OctopusRecommendationEngine/internal/config"
+	"github.com/OctopusSolutionsEngineering/OctopusTerraformTestFramework/octoclient"
+	"github.com/OctopusSolutionsEngineering/OctopusTerraformTestFramework/test"
+	"path/filepath"
+	"testing"
+)
+
+func TestInsecureK8s(t *testing.T) {
+	testFramework := test.OctopusContainerTest{}
+	testFramework.ArrangeTest(t, func(t *testing.T, container *test.OctopusContainer, client *client.Client) error {
+		// Act
+		newSpaceId, err := testFramework.Act(
+			t,
+			container,
+			filepath.Join("..", "..", "..", "test", "terraform"), "31-insecurek8s", []string{})
+
+		if err != nil {
+			return err
+		}
+
+		newSpaceClient, err := octoclient.CreateClient(container.URI, newSpaceId, test.ApiKey)
+
+		if err != nil {
+			return err
+		}
+
+		check := NewOctopusInsecureK8sCheck(
+			newSpaceClient,
+			&config.OctolintConfig{},
+			checks.OctopusClientPermissiveErrorHandler{})
+
+		result, err := check.Execute()
+
+		if err != nil {
+			return err
+		}
+
+		// Assert
+		if result == nil || result.Severity() != checks.Warning {
+			return errors.New("check should have failed")
+		}
+
+		return nil
+	})
+}
